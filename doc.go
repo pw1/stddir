13c824133568b0f9fe758ee4e5175c0614430ca5
Paul Wagenaars <paul@wagenaars.org>
2018-12-29 19:25:56 +0100

    Added doc.go with package documentation.

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package stddir provides cross-platform functions for finding standard directories, such as
+// configuration directories, on that platform. These standard directories include both
+// user-specific and system wide directories.
+package stddir
